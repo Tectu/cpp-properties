530c3d68a57e3b879d2135adc0820ba3d0c3ce54
Joel Bodenmann <joel.bodenmann@simulton.com>
2021-06-04 22:01:50 +0200

    WIP: Qt widgets

--- examples/qt_widgets/main.cpp
+++ examples/qt_widgets/main.cpp
@@ -12,12 +12,24 @@ struct shape :
     MAKE_PROPERTY(enabled, bool);
     MAKE_PROPERTY(x, int);
     MAKE_PROPERTY(y, int);
+
+    shape()
+    {
+        enabled.register_observer([](){ std::cout << "enabled changed!\n"; });
+        x.register_observer([](){ std::cout << "x changed!\n"; });
+        y.register_observer([](){ std::cout << "x changed!\n"; });
+    }
 };
 
 struct circle :
     shape
 {
     MAKE_PROPERTY(radius, int);
+
+    circle()
+    {
+        radius.register_observer([](){ std::cout << "radius channged!\n"; });
+    }
 };
 
 int main(int argc, char* argv[])

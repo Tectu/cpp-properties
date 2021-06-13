1837e1701113b1a278134ff74a8bc01b701c29e8
Joel Bodenmann <joel.bodenmann@simulton.com>
2021-06-13 02:29:10 +0200

    Qt widgets: load & store values

--- cppproperties/qt_widgets/boolean.cpp
+++ cppproperties/qt_widgets/boolean.cpp
@@ -6,4 +6,9 @@ using namespace tct::properties::qt_widgets;
 boolean::boolean(tct::properties::property<bool>& p) :
     widget_base(p)
 {
+    setChecked(p.data);
+
+    connect(this, &QCheckBox::toggled, [this](const bool checked){
+        m_property = checked;
+    });
 }
--- cppproperties/qt_widgets/integer.cpp
+++ cppproperties/qt_widgets/integer.cpp
@@ -5,4 +5,9 @@ using namespace tct::properties::qt_widgets;
 integer::integer(tct::properties::property<type>& p) :
     widget_base(p)
 {
+    setValue(p.data);
+
+    connect(this, qOverload<int>(&QSpinBox::valueChanged), [this](const int value){
+        m_property = value;
+    });
 }

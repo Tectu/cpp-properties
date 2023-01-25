470f2494c8fb3a4df9d8dcd07ac3192819b15bc9
Joel Bodenmann <jbo@insane.engineer>
2023-01-26 00:15:34 +0100

    qt_widgets: make header-only

--- cppproperties/qt_widgets/boolean.hpp
+++ cppproperties/qt_widgets/boolean.hpp
@@ -15,7 +15,16 @@ namespace tct::properties::qt_widgets
         Q_DISABLE_COPY_MOVE(boolean)
 
     public:
-        boolean(tct::properties::property<type>& p);
+        boolean(tct::properties::property<type>& p) :
+            widget_base(p)
+        {
+            setChecked(p.data);
+
+            connect(this, &QCheckBox::toggled, [this](const bool checked){
+                m_property = checked;
+            });
+        }
+
         ~boolean() = default;
     };
 
